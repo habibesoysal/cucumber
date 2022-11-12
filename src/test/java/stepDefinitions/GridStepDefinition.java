572246ae8b5278317e5a64765118cd143e364454
Busra Bozdemir <busratercan4@gmail.com>
2022-11-12 14:15:55 +0300

    12.11.22

---
+++ src/test/java/stepDefinitions/GridStepDefinition.java
@@ -0,0 +1,44 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class GridStepDefinition {
+
+    WebDriver driver;
+
+    @Given("user is on the application_url {string}")
+    public void user_is_on_the_application_url(String url) throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL("http://192.168.1.25:4444"),new ChromeOptions());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get(url);
+
+    }
+    @Then("verify the page title is {string}")
+    public void verify_the_page_title_is(String title) {
+        Assert.assertEquals(title, driver.getTitle());
+    }
+    @Then("close the remote driver")
+    public void close_the_remote_driver() {
+        driver.close();
+    }
+
+    @Given("user is on the application_url with edge {string}")
+    public void userIsOnTheApplication_urlWithFirefox(String url) throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL("http://192.168.1.25:4444"),new EdgeOptions());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+}
